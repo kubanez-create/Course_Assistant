aae3655265bab7ec4e558595b12e860c69d4e8bd
kubanez-create <zo22@mail.ru>
2023-08-08 17:38:07 +0400

    added df refresh method

--- gsheets_db.py
+++ gsheets_db.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import date, datetime, timedelta
 
 import gspread
 import pandas as pd
@@ -14,6 +14,8 @@ class Wb:
         sheet - number of sheet we need;
         key - key (id) for a workbook.
         """
+        self._refresh_time = datetime.now()
+
         gc = gspread.service_account(filename=creds)
         try:
             sheet = gc.open(name)
@@ -23,29 +25,48 @@ class Wb:
         df = pd.DataFrame.from_records(self.sheet_instance.get_all_values())
         df.columns = df.iloc[0]
         df.drop([0], inplace=True)
-        # set format properly when it's consistent across the databases
         df["Дата"] = pd.to_datetime(
             df["Дата"], format="mixed", dayfirst=True
         ).dt.to_pydatetime()
         self.dataframe = df
 
+    def refresh_df(self):
+        """Check whether 3 hours have passed since last df update.
+
+        If the answer yes, fetch info from gsheets, if no - do nothing.
+        There's no point in webinar if it starts in just 3 hours time.
+        Everyone should know all about it at least a day before.
+        """
+        if (self._refresh_time + timedelta(minutes=180)) < datetime.now():
+            df = pd.DataFrame.from_records(self.sheet_instance.get_all_values())
+            df.columns = df.iloc[0]
+            df.drop([0], inplace=True)
+            df["Дата"] = pd.to_datetime(
+                df["Дата"], format="mixed", dayfirst=True
+            ).dt.to_pydatetime()
+            self.dataframe = df
+            self._refresh_time = datetime.now()
+
     def get_future_webs(self):
         """Get future events and return it in handy format, ex. - pandas df."""
+        self.refresh_df()
         return self.dataframe.loc[
-            self.dataframe["Дата"] >= datetime.datetime.today(), "Дата"
+            self.dataframe["Дата"] >= datetime.today(), "Дата"
         ]
 
     def get_web_link(self, web_time):
         """Get link to a webinar date/time."""
+        self.refresh_df()
         return self.dataframe.loc[
             self.dataframe["Дата"] == web_time, "Ссылка"
         ].to_list()[0]
 
     def get_users_db(self):
         """Get all users subscribed to today's events."""
+        self.refresh_df()
         self.dataframe.sort_values(by=["Дата"], inplace=True)
         return self.dataframe.loc[
-            self.dataframe["Дата"].dt.date == datetime.date.today(),
+            self.dataframe["Дата"].dt.date == date.today(),
             ["Никнейм", "ID", "Дата"],
         ].to_records()
 
